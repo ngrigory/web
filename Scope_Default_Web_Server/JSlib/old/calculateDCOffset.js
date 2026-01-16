385cc8d851b831defbbff704e1a55ea3a797f6c0
nonagrigoryan <nonagrigoryan2020@gmail.com>
2026-01-16 05:13:30 -0800

    cleaned proj

--- Scope_Default_Web_Server/JSlib/old/calculateDCOffset.js
+++
@@ -1,5 +0,0 @@
-function calculateDCOffset(signal) {
-    let sum = signal.reduce((total, value) => total + value, 0);
-    let offset = sum / signal.length;
-    return offset;
-}
--- Scope_Default_Web_Server/JSlib/old/calculateRMS.js
+++
@@ -1,5 +0,0 @@
-function calculateRMS(signal) {
-    let dcOffset = calculateDCOffset(signal);
-    let sumOfSquares = signal.reduce((sum, value) => sum + (value - dcOffset) ** 2, 0);
-    return Math.sqrt(sumOfSquares / signal.length);
-}
